b0bbcdccaaef0677f4020400ccb6a87f86e688f2
Kim <hyunjin1612@gmail.com>
2021-08-20 23:40:02 +0900

    init

---
+++ src/main/kotlin/com/github/hyunjin/vsgame/VsgameApplication.kt
@@ -0,0 +1,11 @@
+package com.github.hyunjin.vsgame
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VsgameApplication
+
+fun main(args: Array<String>) {
+	runApplication<VsgameApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/hyunjin/vsgame/VsgameApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.hyunjin.vsgame
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VsgameApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
