758a0eb44331acc9c67e76540e60798be5894f00
kim-hyunjin <hyunjin1612@gmail.com>
2021-08-27 00:35:42 +0900

    [update] Board, Content N:1양방향 연관관계로 수정, 연관메서드 추가, 무한루프를 막기위해 @JsonIgnore 추가

--- src/main/kotlin/com/github/hyunjin/vsgame/domain/board/Board.kt
+++ src/main/kotlin/com/github/hyunjin/vsgame/domain/board/Board.kt
@@ -1,23 +1,41 @@
 package com.github.hyunjin.vsgame.domain.board
 
+import com.fasterxml.jackson.annotation.JsonIgnore
+import org.hibernate.annotations.CreationTimestamp
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
+@SequenceGenerator(
+    name = "BOARD_SEQ_GENERATOR",
+    sequenceName = "BOARD_SEQ",
+    initialValue = 1, allocationSize = 50
+)
 class Board(
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "BOARD_SEQ_GENERATOR")
     @Column(name = "BOARD_ID")
     var id: Long? = null,
 
     @Column(nullable = false)
     var title: String,
-
-    @OneToMany(targetEntity = Content::class, fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
-    @JoinColumn(name = "BOARD_ID")
-    var contents: List<Content>,
-
     var writer: String?,
     var description: String?
-)
+) {
+    @OneToMany(mappedBy = "board", fetch = FetchType.LAZY, cascade = [CascadeType.ALL])
+    @JsonIgnore
+    var contents: MutableList<Content> = ArrayList()
+
+    @CreationTimestamp
+    @Column(name = "created_at", nullable = false, updatable = false)
+    lateinit var createdAt: LocalDateTime
+
+    fun addContent(content: Content) {
+        this.contents.add(content)
+        if (content.board != this) {
+            content.board = this
+        }
+    }
+}
 
 interface BoardRepository : JpaRepository<Board, Long>
--- src/main/kotlin/com/github/hyunjin/vsgame/domain/board/Content.kt
+++ src/main/kotlin/com/github/hyunjin/vsgame/domain/board/Content.kt
@@ -1,16 +1,31 @@
 package com.github.hyunjin.vsgame.domain.board
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import javax.persistence.*
 
 @Entity
+@SequenceGenerator(
+    name = "CONTENT_SEQ_GENERATOR",
+    sequenceName = "CONTENT_SEQ",
+    initialValue = 1, allocationSize = 50
+)
 class Content(
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CONTENT_SEQ_GENERATOR")
     @Column(name = "CONTENT_ID")
-    var id: Long?,
-
-    @Column(name = "BOARD_ID")
-    var boardId: Long?,
-
+    var id: Long? = null,
     var description: String?,
     var photo_url: String?
-)
+) {
+    @ManyToOne
+    @JoinColumn(name = "BOARD_ID")
+    @JsonIgnore
+    var board: Board? = null
+        set(board) {
+            board?.let {
+                field = it
+                if(!it.contents.contains(this)) {
+                    it.contents.add(this)
+                }
+            }
+        }
+}
